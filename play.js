96b6474e9e267f56f67e2c883637318913b90030
yoonjookim <yjblossom@gmail.com>
2023-11-22 18:45:16 -0500

    export- add client

--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const client = require("./client");
 
 console.log("Connecting ...");
-connect();
+client.connect();
