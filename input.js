b26f59073f33d1779b03cdc6bd696f0dd849a28c
yoonjookim <yjblossom@gmail.com>
2023-11-22 22:31:22 -0500

    handleUserInput

--- input.js
+++ input.js
@@ -1,19 +1,41 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+
   return stdin;
 };
 const handleUserInput = function (data) {
   // your code here
   console.log(data);
-
+  let output = "";
   if (data === "\u0003") {
     // If the user presses Ctrl+C (Unicode character \u0003), exit the program
     process.exit();
+  } else {
+    switch (data) {
+      case "w":
+        output = "Move: up";
+        break;
+      case "a":
+        output = "Move: left";
+        break;
+      case "s":
+        output = "Move: down";
+        break;
+      case "d":
+        output = "Move: right";
+        break;
+    }
+    console.log(output);
+    connection.write(output);
   }
+  return output;
 };
 
 module.exports = { setupInput: setupInput };
