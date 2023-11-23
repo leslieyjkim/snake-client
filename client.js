8adc65deba0c45adfdba9df36e9a548a7fb5c32a
yoonjookim <yjblossom@gmail.com>
2023-11-22 21:41:47 -0500

    move up

--- client.js
+++ client.js
@@ -1,6 +1,13 @@
 const net = require("net");
 
 // establishes a connection with the game server
+const moves = {
+  n: "Name: YJ🐍",
+  u: "Move: up",
+  d: "Move: down",
+  r: "Move: right",
+  l: "Move: left",
+};
 
 const connect = function () {
   const conn = net.createConnection({
@@ -8,10 +15,16 @@ const connect = function () {
     port: 50541, // PORT number here,
   });
   conn.on("connect", () => {
-    console.log("Name:  YJ🐍");
     conn.write("Name: YJ🐍");
   });
-  // interpret incoming data as text
+  // conn.write(moves.u);
+  // setTimeout(() => {}, 500);
+  // conn.write(moves.u); // not working
+  setInterval(() => {
+    conn.write("Name: YJ🐍");
+    conn.write(moves.u);
+  }, 100); // one direction continuously
+
   conn.setEncoding("utf8");
   return conn;
 };
